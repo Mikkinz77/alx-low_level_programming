a4910072aa2006b582008144d10ac3dc67512d72
MICHEAL NZOIWU <onzoiwu@gmail.com>
2022-10-19 22:03:16 -0700

    trading

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,5 +1,43 @@
-#ifndef MAIN.H
-#define MAIN.H
-int _putchar (char c)
+Task 0
 
-#endif
+#include "main.h"
+
+
+
+/**
+ *
+ *  * main - Entry point
+ *
+ *   *
+ *
+ *    * Return: Always 0 (Success)
+ *
+ *     */
+
+int main(void)
+
+{
+
+	_putchar('_');
+
+	_putchar('p');
+
+	_putchar('u');
+
+	_putchar('t');
+
+	_putchar('c');
+
+	_putchar('h');
+
+	_putchar('a');
+
+	_putchar('r');
+
+	_putchar('\n');
+
+
+
+	return (0);
+
+}
