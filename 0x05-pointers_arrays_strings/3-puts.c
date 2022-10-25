f4d97c8420628808bf1a32fc275e024fd314ebda
MICHEAL NZOIWU <onzoiwu@gmail.com>
2022-10-24 17:14:35 -0700

    pointed5

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,14 +1,13 @@
 #include "main.h"
 
 /**
- * _puts - Prints a string to stdout.
- * @str: The string to the printed.
+ * _puts - prints a string to stdout.
+ * @str: The string to be printed.
  */
 void _puts(char *str)
 {
 	while (*str)
 		_putchar(*str++);
-
 
 	_putchar('\n');
 }
