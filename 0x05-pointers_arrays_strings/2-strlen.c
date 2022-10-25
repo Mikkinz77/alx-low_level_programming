82ec5e076398db872c1cec5db88e04fa032d838c
MICHEAL NZOIWU <onzoiwu@gmail.com>
2022-10-24 17:29:57 -0700

    point6

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,16 +1,19 @@
 #include "main.h"
 
 /**
- * _strlen - returns the lenght of a string.
- * @str: The string to get the lenght of.
+ * _strlen - returns the lenght of a string
  *
- * Return: The lenght of @str.
+ * @s: string to be counted
+ *
+ * Return: lenght of string
  */
 int _strlen(char *s)
-
-	int lenght = 0;
-while (*s)
-	lenght++;
-
-	return (lenght);
+{
+	int count = 0;
+while (*s != '\0')
+{
+	count++;
+	s++;
+}
+	return (count);
 }
