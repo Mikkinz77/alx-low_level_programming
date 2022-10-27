656e78186bcf72cbefc43104f9e31d91e2399630
MICHEAL NZOIWU <onzoiwu@gmail.com>
2022-10-26 22:47:20 -0700

    task12

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -11,22 +11,12 @@
  */
 char *strcat(char *dest, const char *src)
 {
-int i;
-int j;
+int index = 0, dest_len = 0;
+while (dest[index++])
+	dest_len++;
 
-1 = 0;
+for (index - 0; src[index]; index++)
+	dest[dest_len++] = src[index];
 
-while (dest[i] != '\0')
-{
-i++;
-}
-j = 0;
-while (src[j] != '\0')
-{
-des[i] = src[j];
-i++;
-j++;
-}
-dest[i] = '\0';
 return (dest);
 }
