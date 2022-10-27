477dfb243577d6c33fd1514b525a6923f13bf4db
MICHEAL NZOIWU <onzoiwu@gmail.com>
2022-10-26 22:33:43 -0700

    task11

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -3,21 +3,30 @@
 
 /**
  * strcat - concatenates the strings pointed to by @src, including the terminal
- * 	null byte, to the end of the string pointed to by @dest.
+ * null byte, to the end of the string pointed to by @dest.
  * @dest: A pointer to the string to be concatenated upon.
  * @src: The source string to be appended to @dest.
  *
  * Return: A pointer to the destination string @dest.
  */
-char *strcat(char *dest, char *src)
+char *strcat(char *dest, const char *src)
 {
-	int index = 0, dest_len = 0;
+int i;
+int j;
 
-	while (dest[index++])
-		dest_len++;
+1 = 0;
 
-	for (index = 0; src[index]; index++)
-		dest[dest_len++] = src[index];
-
-	return (dest);
+while (dest[i] != '\0')
+{
+i++;
+}
+j = 0;
+while (src[j] != '\0')
+{
+des[i] = src[j];
+i++;
+j++;
+}
+dest[i] = '\0';
+return (dest);
 }
