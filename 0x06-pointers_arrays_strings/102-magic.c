982739d4fed38639fb54f7874dd20767e3943fd2
MICHEAL NZOIWU <onzoiwu@gmail.com>
2022-10-28 16:00:45 -0700

    A dream doesn't become reality through magic

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,23 +1,23 @@
-#include "main.h"
+#include <stdio.h>
 
+int main(void)
+{
+int n;
+int a[5];
+int *p;
+a[2] = 1024;
+
+p = &n;
 /**
- * _strcmp - compares two strings
- * @s1: first string.
- * @s2: second string.
- * Return: 0 if s1 and s2 are equals,
- * another number if not.
+ * write your line of code here...
+ * Remember:
+ * - you are not allowed to use a
+ * - you are not allowed to modify p
+ * - only one statement
+ * - you are not allowed to code anything else than this line of code
  */
-
-	int _strcmp(char *s1, char *s2)
-{
-int i;
-
-for (i = 0; s1[i] != '\0' || s2[i] != '\0'; i++)
-{
-if (s1[i] != s2[i])
-{
-return (s1[i] - s2[i]);
-}
-}
+*(p + 5) = 98;
+/* ...so that this prints 98\n */
+printf("a[2] = %d\n", a[2]);
 return (0);
 }
