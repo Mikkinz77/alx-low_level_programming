a3f1bbd3f529043c832f3712efd4f6512bd12b99
MICHEAL NZOIWU <onzoiwu@gmail.com>
2022-10-28 12:34:58 -0700

    function that copies a string

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,19 +1,28 @@
 #include "main.h"
 
+
 /**
- * _strcat - Concatenates two strings.
- * @dest: The string to be concatenated upon.
- * @src: The string to be appended to dest.
+ * _strncpy - Copies at most an inputted number
+ *
+ * of bytes from string src into dest.
+ *
+ * @dest: The buffer storing the string copy.
+ *
+ * @src: The source string.
+ *
+ * @n: The maximum number of bytes to copied from src.
  *
  * Return: A pointer to the resulting string dest.
  */
-char *_strncpy(char *dest, char *src, int n)
+	char *_strncpy(char *dest, char *src, int n)
 {
-	int index = 0, dest_len = 0;
+	int index = 0, src_len = 0;
 
-while (dest[index++])
-	dest_len++;
-for (index = 0; src[index]; index++)
-	dest[dest_len++] = src[index];
-return (dest);
+	while (src[index++])
+		src_len++;
+	for (index = 0; src[index] && index < n; index++)
+		dest[index] = src[index];
+	for (index = src_len; index < n; index++)
+		dest[index] = '\0';
+	return (dest);
 }
