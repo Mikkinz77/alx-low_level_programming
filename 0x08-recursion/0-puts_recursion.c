52948863021c681cbd40043ce90934e1116b9cbd
MICHEAL NZOIWU <onzoiwu@gmail.com>
2022-11-02 16:47:53 -0700

    0-puts_recursion.c o

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,27 +1,18 @@
 #include "main.h"
 
 
-
 /**
- * factorial - Returns the factorial of a given number.
- * @n: The number to find the factor of.
- * Return: If n > 0 - the factorial of n.
- * If n < 0 - 1 to indicate an error.
+ * _put_recursion -prints a string, folloewed by a new line.
+ * @s: The string to be printed.
  */
-int factorial(int n)
-
+void _put_recursion(char *s)
 {
-int result = n;
+if (*s)
+{
+_putchar(*s);
+_put_recursion(s + 1);
+}
 
-if (n < 0)
-
-return (-1);
-
-else if (n >= 0 && n <= 1)
-
-return (1);
-
-result *= factorial(n - 1);
-
-return (result);
+else
+_putchar('\n');
 }
