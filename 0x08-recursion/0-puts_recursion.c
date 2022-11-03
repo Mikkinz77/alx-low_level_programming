d5ce9bbbd17dbd7486216b75a6db7fc6d5de6522
MICHEAL NZOIWU <onzoiwu@gmail.com>
2022-11-02 17:17:57 -0700

    task recruision

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -2,7 +2,7 @@
 
 
 /**
- * _put_recursion -prints a string, folloewed by a new line.
+ *_put_recursion -prints a string, folloewed by a new line.
  * @s: The string to be printed.
  */
 void _put_recursion(char *s)
@@ -10,9 +10,11 @@ void _put_recursion(char *s)
 if (*s)
 {
 _putchar(*s);
+
 _put_recursion(s + 1);
 }
 
 else
+
 _putchar('\n');
 }
