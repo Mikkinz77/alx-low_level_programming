c5e7582c572cb42c8facc30d60ed23b7dd0bd8a4
MICHEAL NZOIWU <onzoiwu@gmail.com>
2022-11-03 01:01:17 -0700

    Your subconscious is looking for the dreamer

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -10,13 +10,13 @@
  */
 int find_sqrt(int num, int root)
 {
-	if ((root * root) == num)
-		return (root);
-	
-	if (root == num / 2)
-		return (-1);
-	
-	return (find_sqrt(num, root + 1));
+if ((root * root) == num)
+return (root);
+
+if (root == num / 2)
+return (-1);
+
+return (find_sqrt(num, root + 1));
 }
 
 /**
@@ -27,13 +27,13 @@ int find_sqrt(int num, int root)
  */
 int _sqrt_recursion(int n)
 {
-	int root = 0;
-	
-	if (n < 0)
-		return (-1);
-	
-	if (n == 1)
-		return (1);
-	
-	return (find_sqrt(n, root));
+int root = 0;
+
+if (n < 0)
+return (-1);
+
+if (n == 1)
+return (1);
+
+return (find_sqrt(n, root));
 }
