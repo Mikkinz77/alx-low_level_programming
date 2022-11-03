1997769a67cc43d3f13906e2b3ef68b8d3ca90e3
MICHEAL NZOIWU <onzoiwu@gmail.com>
2022-11-02 17:38:23 -0700

    Requirements o

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -2,7 +2,7 @@
 
 
 /**
- *_put_recursion -prints a string, folloewed by a new line.
+ *_put_recursion - Prints a string, folloewed by a new line.
  * @s: The string to be printed.
  */
 void _put_recursion(char *s)
@@ -11,7 +11,7 @@ if (*s)
 {
 _putchar(*s);
 
-_put_recursion(s + 1);
+_puts_recursion(s + 1);
 }
 
 else
