be1dd48553c90a942bf8f1a770530e8be998beaa
MICHEAL NZOIWU <onzoiwu@gmail.com>
2022-11-08 14:54:45 -0800

    2-str_concat.c two

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -18,31 +18,24 @@ char *ptr, *ret;
 ptr = s1;
 
 if (s1)
-
 while (*ptr++)
-
 size1++;
 
 else
-
 s1 = "";
 
 ptr = s2;
 
 if (s2)
-
 while (*ptr++)
-
 size2++;
 
 else
-
 s2 = "";
 
 ret = malloc(size1 + size2 + 1);
 
 if (!ret)
-
 return (NULL);
 
 ptr = ret;
@@ -56,5 +49,4 @@ while (*s2)
 *ptr = 0;
 
 return (ret);
-
 }
